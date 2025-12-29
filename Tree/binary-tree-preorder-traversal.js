3ba7602e64d5e594bd744f8f2994f191c1321e14
pragyan-satapathy <pragyans248@gmail.com>
2025-12-29 21:17:26 +0530

    pre-order traversal

---
+++ Tree/binary-tree-preorder-traversal.js
@@ -0,0 +1,29 @@
+//  https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (!root) return []
+    let ans = []
+    let stack = [root]
+
+    while (stack.length) {
+        let curr = stack.pop()
+        ans.push(curr.val)
+        curr.right && stack.push(curr.right)
+        curr.left && stack.push(curr.left)
+    }
+    return ans
+    
+};
